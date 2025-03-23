32a15d1dd4da61407ebaa9332608bf73c99206e6
Sriram1233 <sriram.kokkirala@sasi.ac.in>
2025-03-23 17:45:31 +0530

    port

--- practi/app.py
+++ practi/app.py
@@ -20,4 +20,4 @@ def register():
 
     return render_template('index.html')
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5005, debug=True)
+    app.run(host="0.0.0.0", port=5050, debug=True)
